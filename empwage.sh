c56c00dfd7f8b7682a3cdce90043b7123a76475a
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-01-08 22:50:46 +0530

    CHANGED: Employee present and absent

--- empwage.sh
+++ empwage.sh
@@ -1,3 +1,15 @@
-#! /bin/bash
+#! /bin/bash -x
 
 echo " employee wage problems "
+#define constants
+Is_Present=1
+
+#define variable
+empCheck=$((RANDOM%2))
+
+if [ $Is_Present -eq $empCheck ]
+then
+	echo " Employee is Present"
+else
+	echo "Employee is absent"
+fi
